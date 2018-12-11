e2af9dfbcc20d5f9b40ed13f4a6de471231b049f
Bruno Silva <fernando23296@gmail.com>
2018-12-11 16:33:45 -0400

    Commit

--- mlxscoliosis/sistema/flaskblog/__init__.py
+++ mlxscoliosis/sistema/flaskblog/__init__.py
@@ -36,8 +36,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 #app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 #app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-app.config['MAIL_USERNAME'] = 'bruno23296@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Ferdinand1996'
+
 
 
 
